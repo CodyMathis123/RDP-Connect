372658430745da622dd50d103571617a46acb491
Cody Mathis <cody.mathis123@gmail.com>
2020-03-23 23:35:07 -0400

    Update and rename SQL-StoredProcedure.sql to SQL-StoredProcedures.sql

--- SQL-StoredProcedures.sql
+++ SQL-StoredProcedures.sql
@@ -17,3 +17,20 @@ AS
 		, LastLoggedOnUser
 	FROM [dbo].Clients WHERE LastLoggedOnUser LIKE @UserNameFilter
 GO  
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Cody Mathis
+-- Create date: 2020-03-23
+-- Description:	Return a list of LastLoggedOnUser from the Client Health DB for the specified username with a 'like' filter to allow for wildcards
+-- =============================================
+CREATE PROCEDURE GetAlternateUsernames
+    @UserNameFilter nvarchar(50) 
+AS   
+    SET NOCOUNT ON;  
+	SELECT DISTINCT LastLoggedOnUser
+	FROM [dbo].Clients WHERE LastLoggedOnUser LIKE @UserNameFilter
+GO 
